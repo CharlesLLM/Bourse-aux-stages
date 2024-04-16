a0425d1379b10fe92fc323d6acb6915bd0a757ef
MASSON Charlie <charlie.mass.cm@gmail.com>
2024-04-16 19:45:05 +0200

    call api searchbar

--- front/src/components/Searchbar.jsx
+++ front/src/components/Searchbar.jsx
@@ -1,12 +1,16 @@
 import { Combobox } from "react-widgets";
+import {useEffect, useMemo, useState} from "React";
 
 function Searchbar() {
-    const cities = [
-        { id: 0, name: 'Paris'},
-        { id: 1, name: 'Marseille'},
-        { id: 2, name: 'Lille' },
-        { id: 3, name: 'Toulouse' },
-      ];
+    const [cities, setCities] = useState([]);
+
+    useEffect(() => {
+        try{
+            fetch(`https://geo.api.gouv.fr/communes?fields=nom,code,codesPostaux`).then(res =>res.json().then(a =>setCities(a)))
+        } catch(error) {
+            console.log("Erreur: " + error);
+        }
+    }, []);
 
     return (
         <div className="flex flex-row gap-5 bg-slate-50 px-8 py-2 max-w-3xl items-center">
@@ -18,12 +22,18 @@ function Searchbar() {
 
             <div className="flex flex-row gap-2">
                 <div> icon </div>
-                <Combobox
-                    data={cities}
-                    dataKey="id"
-                    textField="name"
-                    placeholder="Sélectionner une ville"
-                />
+                {cities.length === 0 ? (
+                    <Combobox busy placeholder="Chargement..." />
+                ) : (
+                    <Combobox
+                        data={cities}
+                        dataKey="code"
+                        textField="nom"
+                        //textField={item => typeof item === 'string' ? item : item.nom + ' ' + item.codepostaux}
+                        placeholder="Sélectionner une ville"
+                        filter='contains'
+                    />
+                )}
             </div> 
             
             <button className="bg-darkBlue px-6 py-2 text-white"> Rechercher </button>
