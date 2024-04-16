7fd932d2c2c00c5d7953ddde0198e4936c637df5
allan <arondeau@wemanity.com>
2024-04-16 08:47:23 +0200

    chores: switch to vite for react add react create the architecture

---
+++ front/src/main.jsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from "../../front/src/pages/home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
---
+++ front/src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from "../layout/layout.jsx";
+
+function Home() {
+  return (
+    <Layout>
+      <div>
+        <p className="underline">hello</p>
+      </div>
+    </Layout>
+  )
+}
+
+export default Home;
