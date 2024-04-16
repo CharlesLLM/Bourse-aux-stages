4cac6d94c4f40d34b034dc5c91e1e246a1b3cb0e
allan <arondeau@wemanity.com>
2024-04-16 10:34:02 +0200

    chores: fix method to call the router

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,19 +1,10 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import "./index.css"
 
-import Home from "../../front/src/pages/home";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
