3b7963b2a001a58d61f943e170d4f8646c88cfb4
CharlesLLM <charlesllm@orange.fr>
2024-04-28 17:07:30 +0200

    Cruds
    Signed-off-by: CharlesLLM <charlesllm@orange.fr>

---
+++ back/src/Controller/Admin/AdminCrudController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Admin;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class AdminCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Admin::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            AssociationField::new('user', 'common.user'),
+            TextField::new('position', 'admin.position'),
+            AssociationField::new('company', 'common.company'),
+        ];
+    }
+}
---
+++ back/src/Controller/Admin/CompanyCrudController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Company;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class CompanyCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Company::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name', 'common.name'),
+            TextField::new('siret', 'company.siret'),
+            TextField::new('xLink', 'company.x_link'),
+            TextField::new('linkedinLink', 'company.linkedin_link'),
+        ];
+    }
+}
