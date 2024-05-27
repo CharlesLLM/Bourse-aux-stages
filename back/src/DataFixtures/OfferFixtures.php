8810e6d7cff2405b6ce4e1c9714af9e012f6ba70
CharlesLLM <charlesllm@orange.fr>
2024-05-28 00:12:32 +0200

    Min code property
    Signed-off-by: CharlesLLM <charlesllm@orange.fr>

--- back/src/DataFixtures/OfferFixtures.php
+++ back/src/DataFixtures/OfferFixtures.php
@@ -10,7 +10,7 @@ class OfferFixtures extends Fixture
 {
     public const FIXTURE_RANGE = 50;
 
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
         OfferFactory::createMany(self::FIXTURE_RANGE);
 
