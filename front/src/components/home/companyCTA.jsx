4eee0b4e44bd9494e057fddbc072acd6cdabb6a4
allan <arondeau@wemanity.com>
2024-05-31 11:45:18 +0200

    Feat: Reponsive

--- front/src/components/home/companyCTA.jsx
+++ front/src/components/home/companyCTA.jsx
@@ -5,15 +5,15 @@ function CompanyCTA(){
 
 
   return (
-    <div className={` relative overflow-hidden flex flex-row md:flex-row items-center justify-center bg-primary px-8 w-[88vw] space-x-4 `}
-      style={{clipPath: "polygon(20% 0, 100% 0, 100% 85%, 80% 100%, 0 100%, 0 15%)"}}>
-      <div className={`flex flex-col space-y-6 w-[30vw]`}>
-        <h3 className={`text-4xl font-bold text-white`}>Entreprises, déposez vos offres gratuitement</h3>
-        <p className={`text-white`}>Vous pourrez gérer votre planning d'accueil et bénéficier de nombreux service intégrés</p>
+    <div className={` relative overflow-hidden flex flex-col md:flex-row items-center justify-center bg-primary px-8 w-[88vw] space-x-4 `}
+         style={{clipPath: "polygon(20% 0, 100% 0, 100% 85%, 80% 100%, 0 100%, 0 15%)"}}>
+      <div className={`flex flex-col px-16 md:px-2 space-y-6 mt-6 w-full  md:pt-0 md:w-[30vw]`}>
+        <h3 className={`text-3xl lg:text-4xl font-bold text-white`}>Entreprises, déposez vos offres gratuitement</h3>
+        <p className={`text-white text-sm lg:text-md`}>Vous pourrez gérer votre planning d'accueil et bénéficier de nombreux service intégrés</p>
         <Button text={"Créez votre compte"} path={"*"} />
       </div>
-      <div className="pt-28" >
-        <img src="/home/dashboardCompany.svg" className={`w-[45vw]`} alt="dashboard Company" />
+      <div className="pt-12 md:pt-32">
+        <img src="/home/dashboardCompany.svg" className={` w-full md:w-[45vw]`} alt="dashboard Company" />
       </div>
     </div>
   )
