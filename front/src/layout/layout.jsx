ed9e8fcdeacb2abf510bd38012bb9ca16173eab9
allan <arondeau@wemanity.com>
2024-06-03 10:37:01 +0200

    Fix postuler

--- front/src/layout/layout.jsx
+++ front/src/layout/layout.jsx
@@ -4,7 +4,7 @@ import Footer from "../components/nav/footer";
 
 function Layout({children}) {
   return (
-    <div className="w-full h-screen">
+    <div className="w-full min-h-screen">
       <Navbar />
       {children}
       <Footer />
