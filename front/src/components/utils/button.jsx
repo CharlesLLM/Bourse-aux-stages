9bd2de6765d85cf930283cd588f9b7921f2695a3
CharlesLLM <charlesllm@orange.fr>
2024-06-26 11:39:27 +0200

    lint
    Signed-off-by: CharlesLLM <charlesllm@orange.fr>

--- front/src/components/utils/button.jsx
+++ front/src/components/utils/button.jsx
@@ -1,8 +1,7 @@
-import {Link, useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
 import PropTypes from "prop-types";
 
 function Button({text, path, blue = false}) {
-
   return (
     <Link to={path} className={`p-4 w-52 cursor-pointer ${blue ? 'bg-primary' : 'bg-white'}`}>
       <p className={` text-center ${blue ? 'text-white' : 'text-primary'}`}>{text}</p>
