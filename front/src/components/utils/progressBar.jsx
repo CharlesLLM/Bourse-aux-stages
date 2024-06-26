8eaca8a5367438ba1d46c7e7b35e8814a68a766e
CharlesLLM <charlesllm@orange.fr>
2024-06-26 09:28:44 +0200

    lint
    Signed-off-by: CharlesLLM <charlesllm@orange.fr>

--- front/src/components/utils/progressBar.jsx
+++ front/src/components/utils/progressBar.jsx
@@ -1,10 +1,6 @@
-const ProgressBar = ({ offer, viewStyle = false }) => {
-  const duration = Math.floor((new Date(offer.endDate) - new Date(offer.startDate)) / (1000 * 60 * 60 * 24));
-  let durationString = `${duration} jours`;
-  if (duration > 365) {
-    durationString = `${Math.floor(duration / 30)} mois et ${duration % 30} jours`;
-  }
+import PropTypes from 'prop-types';
 
+const ProgressBar = ({ offer, viewStyle = false }) => {
   const publicationDuration = Math.floor((new Date (offer.endPublicationDate) - new Date (offer.createdAt)) / (1000 * 60 * 60 * 24));
   const remainingTime = Math.floor((new Date (offer.endPublicationDate) - new Date()) / (1000 * 60 * 60 * 24));
   const progressBarWidth = remainingTime / publicationDuration * 100;
@@ -25,6 +21,16 @@ const ProgressBar = ({ offer, viewStyle = false }) => {
       </div>
     </div>
   )
+}
+
+ProgressBar.propTypes = {
+  offer: PropTypes.shape({
+    startDate: PropTypes.string,
+    endDate: PropTypes.string,
+    endPublicationDate: PropTypes.string,
+    createdAt: PropTypes.string,
+  }),
+  viewStyle: PropTypes.bool,
 }
 
 export default ProgressBar;
