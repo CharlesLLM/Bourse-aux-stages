f8ec2e7e93a25813fa69dabe59b5220bb49454fc
CharlesLLM <charlesllm@orange.fr>
2024-06-29 15:56:58 +0200

    Min security improvement
    Signed-off-by: CharlesLLM <charlesllm@orange.fr>

--- back/src/Controller/Api/DefaultController.php
+++ back/src/Controller/Api/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Api;
 
 use App\Entity\Company;
+use App\Repository\AdminRepository;
 use App\Repository\CompanyRepository;
 use App\Repository\TagRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -19,6 +20,18 @@ class DefaultController extends AbstractController
     public function __construct(
         private SerializerInterface $serializer
     ) {
+    }
+
+    #[Route('/check-admin', name: 'admin_check_admin', methods: ['GET'])]
+    public function checkAdmin(AdminRepository $adminRepository): JsonResponse
+    {
+        $this->getUser();
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        if (!$this->getUser()->getCompanyAdmin()) {
+            return $this->json(['error' => 'You are not an admin'], 403);
+        }
+
+        return new JsonResponse($this->serializer->serialize($this->getUser()->getCompanyAdmin(), 'json', ['groups' => ['admin']]), Response::HTTP_OK, [], true);
     }
 
     #[Route('/tags', name: 'admin_tags', methods: ['GET'])]
--- front/src/layout/adminLayout.jsx
+++ front/src/layout/adminLayout.jsx
@@ -1,9 +1,39 @@
 import Navbar from "../components/nav/navbar";
 import Footer from "../components/nav/footer";
 import AdminMenu from "../components/nav/adminMenu";
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 function AdminLayout() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkAdmin = async () => {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_BACK_ENDPOINT}api/check-admin`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        });
+
+        // If status error is 403, it means the user is not logged in or is not an admin
+        if (response.status === 403) {
+          navigate('/');
+        }
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+      } catch (err) {
+        console.error('Error fetching data: ', err);
+      }
+    };
+
+    checkAdmin();
+  }, []);
+
   return (
     <div className="w-full min-h-screen">
       <Navbar />
