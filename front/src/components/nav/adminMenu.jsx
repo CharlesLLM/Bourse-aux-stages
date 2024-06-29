432b5fda973426289c6bbf8bbb8ea72cd68c0b9b
CharlesLLM <charlesllm@orange.fr>
2024-06-29 19:21:34 +0200

    Fix login, and improve admin side menu and security
    Signed-off-by: CharlesLLM <charlesllm@orange.fr>

--- front/src/components/nav/adminMenu.jsx
+++ front/src/components/nav/adminMenu.jsx
@@ -1,35 +1,64 @@
 import { Link } from "react-router-dom";
 import { PiBuildings } from "react-icons/pi";
-import { TbMessage2 } from "react-icons/tb";
+import { TbMessage2, TbSettings } from "react-icons/tb";
 import { IoMdPeople } from "react-icons/io";
 import { LuClipboardList } from "react-icons/lu";
+import PropTypes from 'prop-types';
 
-function AdminMenu() {
+function AdminMenu({ admin }) {
   return (
-    <div className="flex flex-col gap-8 h-full bg-lightGrey font-medium text-[#7C8493] py-8 border-r border-borderGrey">
+    <div className="flex flex-col gap-8 h-full bg-lightGrey font-medium text-[#7C8493] py-8 border-r border-borderGrey w-72">
       <Link to="/admin">
         <h2 className="title text-black text-2xl ml-8">Mon compte</h2>
       </Link>
-      <div>
-        <Link to="/admin/entreprise/mentalworks" className="w-full flex items-center gap-4 py-3 pl-8">
+      <div className="pl-8 pr-4">
+        <Link to={`/admin`} className="flex items-center gap-4 py-3">
           <TbMessage2 className="size-6"/>
           <p>Messages</p>
         </Link>
-        <Link to="/admin/entreprise/mentalworks" className="w-full flex items-center gap-4 py-3 pl-8">
+        <Link to={`/admin/entreprise/${admin.company.slug}`} className="flex items-center gap-4 py-3">
           <PiBuildings className="size-6"/>
           <p>Fiche entreprise</p>
         </Link>
-        <Link to="/admin/entreprise/mentalworks" className="w-full flex items-center gap-4 py-3 pl-8">
+        <Link to={`/admin`} className="flex items-center gap-4 py-3">
           <IoMdPeople className="size-6" />
           <p>Candidatures</p>
         </Link>
-        <Link to="/admin/entreprise/mentalworks" className="w-full flex items-center gap-4 py-3 pl-8">
+        <Link to={`/admin`} className="flex items-center gap-4 py-3">
           <LuClipboardList className="size-6" />
           <p>Offres</p>
         </Link>
       </div>
+      <hr className="h-px bg-borderGrey" />
+      <div className="pl-8 pr-4 space-y-5">
+        <Link to="/admin" className="flex items-center gap-4">
+          <TbSettings className="size-6"/>
+          <p>Paramètres</p>
+        </Link>
+        {/* Admin picture + name */}
+        <Link to="/admin/profil" className="flex items-center gap-4 py-3">
+          <img src={admin.picture} alt="Admin" className="w-12 h-12 rounded-full"/>
+          <div>
+            <p className="text-black text-lg font-semibold">{admin.user.firstName} <span className="uppercase">{admin.user.lastName}</span></p>
+            <p className="text-textGrey text-sm">Administrateur</p>
+          </div>
+        </Link>
+      </div>
     </div>
   )
+}
+
+AdminMenu.propTypes = {
+  admin: PropTypes.shape({
+    picture: PropTypes.string,
+    user: PropTypes.shape({
+      firstName: PropTypes.string,
+      lastName: PropTypes.string
+    }),
+    company: PropTypes.shape({
+      slug: PropTypes.string.isRequired
+    }).isRequired
+  }).isRequired
 }
 
 export default AdminMenu;
--- front/src/pages/admin/adminDashboard.jsx
+++ front/src/pages/admin/adminDashboard.jsx
@@ -1,45 +1,10 @@
-import { useEffect, useState } from "react";
 import Container from "../../layout/container.jsx";
 
 function AdminDashboard() {
-  const [admin, setAdmin] = useState({});
-
-  useEffect(() => {
-    const getAdmin = async () => {
-      try {
-        const response = await fetch(`${import.meta.env.VITE_BACK_ENDPOINT}api/admin/dashboard`, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${localStorage.getItem('token')}`,
-          },
-        });
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        setAdmin(data);
-      } catch (err) {
-        console.error('Error fetching data: ', err);
-      }
-    };
-
-    getAdmin();
-  }, []);
-
   return (
     <Container className="flex-col gap-6">
       <h2 className="text-4xl">Bienvenue dans votre espace administrateur</h2>
-      <span className="block w-full h-0.5 bg-grey/50"></span>
-      {admin && admin.user && (
-        <div className="flex flex-col gap-2">
-          <p className="text-lg">Nom: {admin.user.lastName}</p>
-          <p className="text-lg">Prénom: {admin.user.firstName}</p>
-          <p className="text-lg">Email: {admin.user.email}</p>
-          <p className="text-lg">Entreprise: {admin.company.name}</p>
-        </div>
-      )}
+      <span className="block w-full h-px bg-grey/50"></span>
     </Container>
   )
 }
